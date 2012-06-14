c90b47a9372cba66b5a47b79c30fd48e6efa5cce
deepthi <deepthirera@gmail.com>
2012-06-14 11:35:24 +0530

    Showing the cheaters also

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,16 +1,17 @@
 class VotesController < ApplicationController
-def index
-  @candidates = Candidate.all
-  @vote = Vote.new
-end
-def create
-  Vote.create(:voter_id => params[:vote][:id], :comment => params[:vote][:comment], :candidate_id => params[:vote][:voter_id])
-end
-def results
-  h = {}
-  Candidate.all.each do |candidate| 
-    h["#{candidate.name}"] = candidate.votes.count
+  def index
+    @candidates = Candidate.all
+    @vote = Vote.new
   end
-   @results =  h.max_by{|k,v| v}
-end
+  def create
+    Vote.create(:voter_id => params[:vote][:id], :comment => params[:vote][:comment], :candidate_id => params[:vote][:voter_id])
+  end
+  def results
+    h = {}
+    Candidate.all.each do |candidate|
+      h["#{candidate.name}"] = candidate.votes.count
+    end
+    @results =  h.max_by{|k,v| v}
+    @cheaters = Vote.cheaters
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,7 @@
 class Vote < ActiveRecord::Base
   belongs_to :candidate
+  def self.cheaters
+    cheaters = Candidate.all.collect(&:id)-Vote.all.collect(&:voter_id).uniq
+    cheaters.collect{|x| Candidate.find(x).name}
+  end
 end
